1819b50cdf02ad24d861060347d9c05c95519aef
Robert Brockmann <bbrockmann1@gmail.com>
2022-09-07 11:51:04 -0600

    added render toy card function

--- src/index.js
+++ src/index.js
@@ -12,4 +12,26 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+    fetch('http://localhost:3000/toys')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(renderToyCard)
+      //console.log(data);
+    });
+
+    function renderToyCard(toyObj){ //Render each toy object with new elements.
+      //console.log(toyObj);
+      const div = document.createElement('div');
+      const h2 = document.createElement('h2');
+      const img = document.createElement('img');
+      const p = document.createElement('p');
+      const button = document.createElement('button');
+
+      h2.textContent = toyObj.name;
+      img.src = toyObj.image;
+      p.textContent = `${toyObj.likes} likes`;
+
+      
+    };
 });
+
