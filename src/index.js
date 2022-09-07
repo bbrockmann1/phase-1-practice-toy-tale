7339afe457eca9941428a28156d17a6b0f9f8cd0
Robert Brockmann <bbrockmann1@gmail.com>
2022-09-07 10:10:34 -0600

    updated render you function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
+    // hide & seek with the for
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -13,3 +13,22 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  function fetchToyData(url){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data));
+  }
+  fetchToyData('http://localhost:3000/toys');
+
+  function renderToy(data){
+    const toyContainer = document.getElementById('toy-collection');
+    
+  };
+});
+
+
+
+
+
